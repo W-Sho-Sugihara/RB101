71bc35b8af693f48a4d905128efc733d3645234f
Sho Sugihara <shosmail31@gmail.com>
2021-09-09 20:51:58 -0700

    partly finished with medium_1

--- Small_Problems/Medium_1.rb
+++ Small_Problems/Medium_1.rb
@@ -10,18 +10,19 @@
   #init new array = to input array clone
   #push the return value of shift of new array to new array
 
-def rotate_array(array)
-  arr2 = array.clone
-  arr2.push(arr2.shift)
-end
+# def rotate_array(array)
+#   arr2 = array.clone
+#   arr2 << arr2.shift
+# end
 
-def rotate_string(string)
-  rotate_array(string.chars).join
-end
+# def rotate_string(string)
+#   rotate_array(string.chars).join
+# end
 
-def rotate_integer(integer)
-  rotate_array(integer.to_s.chars).join
-end
+# def rotate_integer(integer)
+#   rotate_array(integer.to_s.chars).join.to_i
+# end
+
 # p rotate_array([7, 3, 5, 2, 9, 1])# == [3, 5, 2, 9, 1, 7]
 # p rotate_array(['a', 'b', 'c']) #== ['b', 'c', 'a']
 # p rotate_array(['a']) #== ['a']
@@ -32,8 +33,8 @@ end
 
 
 
-p rotate_string('asdfgh')
-p rotate_integer(123456)
+# p rotate_string('asdfgh')
+# p rotate_integer(123456)
 
 #----------------ROTATE 2-----------------------
 # input: 2 integers (number & n)
@@ -47,3 +48,44 @@ p rotate_integer(123456)
   # number to string 
   # number [n+1..] pass into rotate_string, add return value to number[0..n]
 
+
+# def rotate_rightmost_digits(number, n)
+#   (number.to_s[0...-n] + rotate_string(number.to_s[-n..])).to_i
+  
+# end
+
+# p rotate_rightmost_digits(12345, 1)
+# p rotate_rightmost_digits(12345, 2)
+# p rotate_rightmost_digits(12345, 3)
+# p rotate_rightmost_digits(12345, 4)
+# p rotate_rightmost_digits(12345, 5)
+
+#-----------ROTATE 3-----------------------
+# input: number
+# output: new number
+# Rules: return maximum rotation
+
+# def max_rotation(number)
+#   (number.to_s.size).downto(1) { |n| number = rotate_rightmost_digits(number, n) }
+#   number.to_i
+# end
+
+# def max_rotation(num)
+#   str = num.to_s
+
+#   (0...str.length).each do |i|
+#     str << str[i] 
+#     str[i] = ""
+#   end
+
+#   str.to_i
+# end
+
+# p max_rotation(1234567) #== 321579
+# p max_rotation(735291) == 321579
+# p max_rotation(3) == 3
+# p max_rotation(35) == 53
+# p max_rotation(105) == 15 # the leading zero gets dropped
+# p max_rotation(8_703_529_146) == 7_321_609_845
+
+#-------------- 1000 Lights ---------------------------
